436c5832008d8cc4de2aa6418fc2554f84867c66
Anthony M <ampats2008@gmail.com>
2022-04-07 14:34:04 -0400

    fixed fallbackData option

--- hooks/useGetTVShows.tsx
+++ hooks/useGetTVShows.tsx
@@ -3,16 +3,16 @@ import useSWR from "swr"
 type Args = {
   pageIndex: number
   sortBy: string
-  initialData: any
+  fallbackData: any
 }
 
-function useGetTVShows({ pageIndex, sortBy, initialData }: Args) {
+function useGetTVShows({ pageIndex, sortBy, fallbackData }: Args) {
   const API_KEY = "b266704b1a8e284b85f455fc1050f942"
   const apiURL = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=${sortBy}&include_adult=false&include_video=false&page=${pageIndex}&with_watch_monetization_types=flatrate`
   //@ts-ignore
   const fetcher = (...args) => fetch(...args).then((res) => res.json())
-  //not sure what this ts warning means:
-  const { data, error } = useSWR(apiURL, fetcher, { initialData })
+
+  const { data, error } = useSWR(apiURL, fetcher, { fallbackData })
 
   return {
     data: data,
