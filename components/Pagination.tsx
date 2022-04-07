4a03dfe00efdd8af1b0ba21d54faa52ed65db22b
Anthony M <ampats2008@gmail.com>
2022-04-07 14:33:34 -0400

    formatted with prettier / added warning

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,5 +1,8 @@
 import * as React from "react"
-import { ArrowCircleRightIcon, ArrowCircleLeftIcon } from '@heroicons/react/solid'
+import {
+  ArrowCircleRightIcon,
+  ArrowCircleLeftIcon,
+} from "@heroicons/react/solid"
 
 type Props = {
   pageIndex: number
@@ -7,27 +10,51 @@ type Props = {
 }
 
 const Pagination: React.FC<Props> = ({ pageIndex, setPageIndex }) => {
-  const btnClass = "transition-colors text-indigo-600 hover:text-indigo-400 dark:text-indigo-400 dark:hover:text-indigo-300"
+  const btnClass =
+    "transition-colors text-indigo-600 hover:text-indigo-400 dark:text-indigo-400 dark:hover:text-indigo-300"
 
   const handlePageChange = (direction: string) => {
     if (direction === "prev") {
-      setPageIndex((prevIndex) => (prevIndex > 1 ? prevIndex - 1 : prevIndex))
+      setPageIndex((prevIndex) => {
+        if (prevIndex > 1) {
+          return prevIndex - 1
+        } else {
+          alert("Can't go back further. You're on the first page.")
+          return prevIndex
+        }
+      })
     } else {
       setPageIndex((prevIndex) => prevIndex + 1)
     }
     window.scrollTo(0, 0)
   }
 
+  // Unused Code for Conditionally rendered Warning Elements
+  /* {pageIndex === 1 && (
+    <h3 className="my-7 text-xl">
+      <ExclamationIcon className="h-6 w-6 inline-block mr-2 dark:text-indigo-400 text-indigo-600" />
+      Can't go back further. You're on the first page.
+    </h3>
+  )}
+  {pageIndex === data.total_pages && (
+    <h3 className="my-7 text-xl">
+      <ExclamationIcon className="h-6 w-6 inline-block mr-2 dark:text-indigo-400 text-indigo-600" />
+      Can't go to next page. You've reached the last page.
+    </h3>
+  )} */
+
   return (
     <>
       <div id="pagination" className="flex flex-nowrap gap-5 justify-center">
         {/* for setPageIndex, if the current page is 1, keep it the same*/}
         <button onClick={() => handlePageChange("prev")} className={btnClass}>
-          <ArrowCircleLeftIcon className="h-10 w-10"/>
+          <ArrowCircleLeftIcon className="h-10 w-10" />
         </button>
-        <span className="p-2 text-xl text-indigo-600 dark:text-indigo-300">{pageIndex}</span>
+        <span className="p-2 text-xl text-indigo-600 dark:text-indigo-300">
+          {pageIndex}
+        </span>
         <button onClick={() => handlePageChange("next")} className={btnClass}>
-          <ArrowCircleRightIcon className="h-10 w-10"/>
+          <ArrowCircleRightIcon className="h-10 w-10" />
         </button>
       </div>
     </>
