2df198e00a5a6e72d531420a037b42aee50d0457
Anthony M <ampats2008@gmail.com>
2022-04-11 12:18:27 -0400

    added favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -148,7 +148,7 @@ export default Home
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
 // direct database queries.
-export async function getStaticProps() {
+export async function getServerSideProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
   const res = await fetch(
